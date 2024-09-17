2993c64d7831e0c7697ba39c08f0b148b3322496
이소희 <isohui@isohuis-Mac-mini.local>
2024-09-17 17:19:59 +0900

    init projects

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,15 @@
-import { baseUrl } from 'app/sitemap'
+import { baseUrl } from "app/sitemap";
+import { MetadataRoute } from "next";
 
-export default function robots() {
+// 크롤러가 어떤 권한으로 어디까지 탐험 할 수 있는지 규칙 문서. 정적.
+
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: '*',
+        userAgent: "*",
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
-  }
+  };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,18 @@
-import { getBlogPosts } from 'app/blog/utils'
+import { getBlogPosts } from "app/blog/utils";
+import { MetadataRoute } from "next";
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = "https://portfolio-blog-starter.vercel.app";
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
-  }))
+  }));
 
-  let routes = ['', '/blog'].map((route) => ({
+  let routes = ["", "/blog"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
-  return [...routes, ...blogs]
+  return [...routes, ...blogs];
 }
