92f2b2526fa7d7d9c22bde8d5c6db09a4a303b94
sohee lee <kksm456@gmail.com>
2025-02-26 03:35:07 +0900

    [new post] 서버 캐싱

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
-import { BlogPosts } from "app/components/posts";
-import { getBlogPosts } from "./post/utils";
+import { BlogPosts } from 'app/components/posts';
+import { getBlogPosts } from './post/utils';
 
 export default function Page() {
-  const recentPosts = getBlogPosts()
-    .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-        return -1;
-      }
-      return 1;
-    })
-    .slice(0, 3);
-  return (
-    <section>
-      <h1 className="font-semibold text-3xl pb-10 tracking-tighter">
-        Soy Blog
-      </h1>
-      <h6 className="font-semibold text-xl pb-5 tracking-tighter">
-        Recent Posts
-      </h6>
-      <BlogPosts posts={recentPosts} />
-    </section>
-  );
+	const recentPosts = getBlogPosts().sort((a, b) => {
+		if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+			return -1;
+		}
+		return 1;
+	});
+
+	return (
+		<section>
+			<h1 className='font-semibold text-3xl pb-10 tracking-tighter'>Soy Blog</h1>
+			<h6 className='font-semibold text-xl pb-5 tracking-tighter'>Recent Posts</h6>
+			<BlogPosts posts={recentPosts} />
+		</section>
+	);
 }
