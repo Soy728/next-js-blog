5ef7e58d6623f72bef24a6bc9f44273e722f5f2b
sohee lee <kksm456@gmail.com>
2025-03-01 06:58:03 +0900

    og image 추가

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,18 @@
-import { getBlogPosts } from "app/post/utils";
-import { MetadataRoute } from "next";
+import { getBlogPosts } from 'app/post/utils';
+import { MetadataRoute } from 'next';
 
-export const baseUrl = "https://portfolio-blog-starter.vercel.app";
+export const baseUrl = 'https://soy728-blog.vercel.app';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
+	let blogs = getBlogPosts().map((post) => ({
+		url: `${baseUrl}/blog/${post.slug}`,
+		lastModified: post.metadata.publishedAt,
+	}));
 
-  let routes = ["", "/blog"].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
-  }));
+	let routes = ['', '/blog'].map((route) => ({
+		url: `${baseUrl}${route}`,
+		lastModified: new Date().toISOString().split('T')[0],
+	}));
 
-  return [...routes, ...blogs];
+	return [...routes, ...blogs];
 }
